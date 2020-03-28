012a9e0dec1fd2ab64acd120c99108d55e928519
Jack <git@jackhiggins.ie>
2020-03-28 14:31:19 +0100

    flash

--- scripts/killswitch.py
+++ scripts/killswitch.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python
 
+import time
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 redPin   = 11
 greenPin = 13
 bluePin  = 15
 
+def lightsOn():
+    GPIO.output(redPin, GPIO.HIGH)
+    GPIO.output(greenPin, GPIO.HIGH)
+    GPIO.output(bluePin, GPIO.HIGH)
+
+def lightsOff():
+    GPIO.output(redPin, GPIO.LOW)
+    GPIO.output(greenPin, GPIO.LOW)
+    GPIO.output(bluePin, GPIO.LOW)
+
 def button_callback(channel):
     print("Button was pushed!")
+    lightsOn()
+    time.sleep(5)
+    lightsOff()
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 
 GPIO.setup(redPin, GPIO.OUT)
-GPIO.output(redPin, GPIO.HIGH)
-
 GPIO.setup(greenPin, GPIO.OUT)
-GPIO.output(greenPin, GPIO.HIGH)
-
 GPIO.setup(bluePin, GPIO.OUT)
-GPIO.output(bluePin, GPIO.HIGH)
 
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
@@ -28,3 +37,4 @@ GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
 GPIO.cleanup() # Clean up
+
