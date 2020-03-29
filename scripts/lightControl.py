0039e7abff0f14f52d7690839dc9bdda7c539b3e
Jack <git@jackhiggins.ie>
2020-03-29 15:42:37 +0200

    script purely to control lights

---
+++ scripts/lightControl.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import sys
+import RPi.GPIO as GPIO
+
+
+# The PIN Numbers - Not the GPIO numbers. See central numbers on https://www.electronicwings.com/raspberry-pi/raspberry-pi-gpio-access
+RED_PIN = 15
+GREEN_PIN = 13
+BLUE_PIN = 11
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(RED_PIN, GPIO.OUT)
+GPIO.setup(GREEN_PIN, GPIO.OUT)
+GPIO.setup(BLUE_PIN, GPIO.OUT)
+
+def printHelp(exitCode):
+    print("Supply a single parameter for colour name [RED, GREEN, BLUE] or OFF.")
+    exit(exitCode)
+
+if(len(sys.argv) < 2):
+	printHelp(1)
+
+def lightsOff():
+    GPIO.output(RED_PIN, GPIO.LOW)
+    GPIO.output(GREEN_PIN, GPIO.LOW)
+    GPIO.output(BLUE_PIN, GPIO.LOW)
+
+def lightOn(chosenLight):
+    GPIO.output(chosenLight, GPIO.HIGH)
+
+
+if(sys.argv[1] == "RED"):
+	print("Red light ON")
+	lightsOff()
+	lightOn(RED_PIN)
+	exit(0)
+
+if(sys.argv[1] == "GREEN"):
+	print("Green light ON")
+    	lightsOff()
+    	lightOn(GREEN_PIN)
+    	exit(0)
+
+if(sys.argv[1] == "BLUE"):
+	print("Blue light ON")
+    	lightsOff()
+    	lightOn(BLUE_PIN)
+    	exit(0)
+
+printHelp(2)
